2cd17b345f1cff7858affd30df5603963fb9fb0f
Imam <imamzuhdimuzakkiy1@gmail.com>
2025-12-12 14:39:13 +0700

    fix: update path resolution in Vite configuration for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import path from "path"
+import path from 'path';
+import { fileURLToPath } from "url";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
-      "@": path.resolve(new URL("./src", import.meta.url).pathname),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
